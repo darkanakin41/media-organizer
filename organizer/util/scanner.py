687505962421472d153f96c8e1e82747968cb81e
Pierre LEJEUNE <darkanakin41@gmail.com>
2020-12-27 11:38:35 +0100

    chore(Tests): full test scanner.py

--- organizer/util/scanner.py
+++ organizer/util/scanner.py
@@ -1,28 +1,38 @@
 import os
 import re
+from typing import List
 
 from organizer import config
 from organizer.util.logger import logger
 
 
-def scan_folder(folder: str):
-    logger.info("Processing folder %s" % folder)
+def scan_folder(folder: str) -> List[str]:
+    """
+    Scan a folder and retrieve all media files
+    :param folder: the folder to scan
+    :return: the list of files found
+    """
+    logger.info("Processing folder %s", folder)
     extensions = config.get('input').get('extensions')
     file_extensions_regex = re.compile(extensions)
 
     found_files = []
+
+    # pylint: disable=unused-variable
     for root, dirs, files in os.walk(folder):
         found_files += [os.path.join(root, f) for f in files if re.match(file_extensions_regex, f)]
 
-    logger.info("Found %d files" % len(found_files))
+    logger.info("Found %d files", len(found_files))
 
     return found_files
 
 
 def is_ignored(file: str) -> bool:
-    if config.get('input').get('ignored') is None:
-        return False
-
+    """
+    Check if the given file is ignored
+    :param file: the file
+    :return: if the file is ignored or not
+    """
     for ignored in config.get('input').get('ignored'):
         ignored_regex = re.compile(ignored)
         if re.match(ignored_regex, file):
--- tests/utils/scanner_test.py
+++ tests/utils/scanner_test.py
@@ -1,6 +1,8 @@
 import os
 
-from organizer.util.scanner import scan_folder
+from organizer.util.scanner import scan_folder, is_ignored
+
+data_folder = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'data')
 
 
 def test_scan_folder_no_results():
@@ -12,10 +14,27 @@ def test_scan_folder_no_results():
     assert len(scan_folder(folder)) == 0
 
 
-def test_scan_folder_one_result():
+def test_scan_folder_results():
     """
     Test one result
     :return:
     """
-    folder = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'data')
-    assert len(scan_folder(folder)) == 1
+    assert len(scan_folder(data_folder)) == 2
+
+
+def test_is_ignored_true():
+    """
+    Test no results
+    :return:
+    """
+    file = os.path.join(data_folder, 'test.ignored.mkv')
+    assert is_ignored(file)
+
+
+def test_is_ignored_false():
+    """
+    Test one result
+    :return:
+    """
+    file = os.path.join(data_folder, 'test.mkv')
+    assert not is_ignored(file)
