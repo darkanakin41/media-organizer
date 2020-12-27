4cf6f0fd51e22db996dd9cce68182acaa8622940
Pierre LEJEUNE <darkanakin41@gmail.com>
2020-12-27 10:57:09 +0100

    chore(Configuration): add more debug

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,13 @@ from organizer.util.config import BASE_FOLDER, get_config
 
 @pytest.fixture(scope="session", autouse=True)
 def execute_before_any_test():
+    """
+    Configuration before any tests
+    :return:
+    """
     os.environ['MEDIA_ORGANIZER_CONFIG'] = os.path.join(BASE_FOLDER, 'tests')
+
+    print('execution of conftest')
 
     sys.argv = ['--verbose']
     get_config()
